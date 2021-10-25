7f90bdb3beaa92048da715305626741760c8a856
H-P <shovongomir@gmail.com>
2021-10-25 14:06:19 -0700

    first commit

---
+++ House Rent/Form1.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Text.RegularExpressions;
+
+
+
+
+
+namespace House_Rent
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+       
+
+        private void button1_Click(object sender, EventArgs e)//exit button
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)// clear button
+        {
+            textBox1.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+            textBox8.Clear();
+            comboBox2.Text = "";
+            comboBox3.SelectedIndex = 0;
+            comboBox4.SelectedIndex = 0;
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            NewMethod();
+        }
+
+        private void NewMethod()
+        {
+            if (textBox3.Text != "" && comboBox4.Text != "" && comboBox3.Text == "1053146" && textBox4.Text != "" && textBox8.Text !="")
+            {
+                decimal total = Convert.ToInt32(textBox3.Text) / 2;
+                textBox5.Text = total.ToString();
+                decimal Total = Convert.ToInt32(textBox3.Text) / 2 + Convert.ToInt32(comboBox4.Text) + Convert.ToInt32(textBox4.Text) - Convert.ToInt32(textBox8.Text);
+                textBox6.Text = Total.ToString();
+
+            }
+            else if (textBox3.Text != "" && comboBox4.Text != "" && comboBox3.Text == "1053146" && textBox4.Text != "" && textBox8.Text == "")
+            {
+                decimal total = Convert.ToInt32(textBox3.Text) / 2;
+                textBox5.Text = total.ToString();
+                decimal Total = Convert.ToInt32(textBox3.Text) / 2 + Convert.ToInt32(comboBox4.Text) + Convert.ToInt32(textBox4.Text) ;
+                textBox6.Text = Total.ToString();
+
+            }
+
+            else if (textBox3.Text != "" && comboBox4.Text != "" && comboBox3.Text == "1053146" && textBox4.Text == "" && textBox8.Text != "")
+            {
+                decimal total = Convert.ToInt32(textBox3.Text) / 2;
+                textBox5.Text = total.ToString();
+                decimal Total = Convert.ToInt32(textBox3.Text) / 2 + Convert.ToInt32(comboBox4.Text) - Convert.ToInt32(textBox8.Text);
+                textBox6.Text = Total.ToString();
+
+            }
+            else if (textBox3.Text != "" && comboBox4.Text != "" && comboBox3.Text == "1053146" && textBox4.Text == "" && textBox8.Text == "")
+            {
+                decimal total = Convert.ToInt32(textBox3.Text) / 2;
+                textBox5.Text = total.ToString();
+                decimal Total = Convert.ToInt32(textBox3.Text) / 2 + Convert.ToInt32(comboBox4.Text) ;
+                textBox6.Text = Total.ToString();
+
+            }
+
+            else if (textBox3.Text != "" && comboBox4.Text != "" && comboBox3.Text == "1050453" && textBox4.Text != "" && textBox8.Text != "")
+            {
+                decimal total = Convert.ToInt32(textBox3.Text) / 3;
+                textBox5.Text = total.ToString();
+                decimal Total = Convert.ToInt32(textBox3.Text) / 3 + Convert.ToInt32(comboBox4.Text) + Convert.ToInt32(textBox4.Text) - Convert.ToInt32(textBox8.Text);
+                textBox6.Text = Total.ToString();
+
+            }
+
+            else if (textBox3.Text != "" && comboBox4.Text != "" && comboBox3.Text == "1050453" && textBox4.Text != "" && textBox8.Text == "")
+            {
+                decimal total = Convert.ToInt32(textBox3.Text) / 3;
+                textBox5.Text = total.ToString();
+                decimal Total = Convert.ToInt32(textBox3.Text) / 3 + Convert.ToInt32(comboBox4.Text) + Convert.ToInt32(textBox4.Text);
+                textBox6.Text = Total.ToString();
+
+            }
+
+
+
+            else if (textBox3.Text != "" && comboBox4.Text != "" && comboBox3.Text == "1050453" && textBox4.Text == "" && textBox8.Text != "")
+            {
+                decimal total = Convert.ToInt32(textBox3.Text) / 3;
+                textBox5.Text = total.ToString();
+                decimal Total = Convert.ToInt32(textBox3.Text) / 3 + Convert.ToInt32(comboBox4.Text) - Convert.ToInt32(textBox8.Text);
+                textBox6.Text = Total.ToString();
+
+            }
+
+            else if (textBox3.Text != "" && comboBox4.Text != "" && comboBox3.Text == "1050453" && textBox4.Text == "" && textBox8.Text == "")
+            {
+                decimal total = Convert.ToInt32(textBox3.Text) / 3;
+                textBox5.Text = total.ToString();
+                decimal Total = Convert.ToInt32(textBox3.Text) / 3 + Convert.ToInt32(comboBox4.Text) ;
+                textBox6.Text = Total.ToString();
+
+            }
+
+
+
+
+            else if (textBox3.Text != "" && comboBox4.Text != "" && comboBox3.Text != "1050453" && comboBox3.Text != "1053146" && textBox4.Text != "" && textBox8.Text != "")
+            {
+                decimal total = Convert.ToInt32(textBox3.Text);
+                textBox5.Text = total.ToString();
+                decimal Total = Convert.ToInt32(textBox3.Text) + Convert.ToInt32(comboBox4.Text) + Convert.ToInt32(textBox4.Text) - Convert.ToInt32(textBox8.Text);
+                textBox6.Text = Total.ToString();
+
+            }
+
+
+            else if (textBox3.Text != "" && comboBox4.Text != "" && comboBox3.Text != "1050453" && comboBox3.Text != "1053146" && textBox4.Text != "" && textBox8.Text == "")
+            {
+                decimal total = Convert.ToInt32(textBox3.Text);
+                textBox5.Text = total.ToString();
+                decimal Total = Convert.ToInt32(textBox3.Text) + Convert.ToInt32(comboBox4.Text) + Convert.ToInt32(textBox4.Text) ;
+                textBox6.Text = Total.ToString();
+
+            }
+
+
+
+            else if (textBox3.Text != "" && comboBox4.Text != "" && comboBox3.Text != "1050453" && comboBox3.Text != "1053146" && textBox4.Text == "" && textBox8.Text != "")
+            {
+                decimal total = Convert.ToInt32(textBox3.Text);
+                textBox5.Text = total.ToString();
+                decimal Total = Convert.ToInt32(textBox3.Text) + Convert.ToInt32(comboBox4.Text) - Convert.ToInt32(textBox8.Text);
+                textBox6.Text = Total.ToString();
+
+            }
+
+
+            else if (textBox3.Text != "" && comboBox4.Text != "" && comboBox3.Text != "1050453" && comboBox3.Text != "1053146" && textBox4.Text == "" && textBox8.Text == "")
+            {
+                decimal total = Convert.ToInt32(textBox3.Text);
+                textBox5.Text = total.ToString();
+                decimal Total = Convert.ToInt32(textBox3.Text) + Convert.ToInt32(comboBox4.Text) ;
+                textBox6.Text = Total.ToString();
+
+            }
+
+
+
+            else { 
+                
+                textBox5.Text = "0";
+                textBox6.Text = "0";
+            }
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)//meter name
+        {
+            NewMethod();
+        }
+
+        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)//house rent
+        {
+            NewMethod();
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)//due
+        {
+            NewMethod();
+        }
+
+        private void textBox8_TextChanged(object sender, EventArgs e)//payment at begining
+        {
+            NewMethod();
+        }
+
+        private void button3_Click(object sender, EventArgs e)//print preview
+        {
+
+            HRprintPreviewDialog.Document = HRprintDocument;
+            HRprintPreviewDialog.ShowDialog();
+        }
+
+        private void HRprintDocument_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            e.Graphics.DrawString("SL No: " + textBox1.Text, new Font("Arial", 12, FontStyle.Bold), Brushes.Blue, new Point(650, 120));//serial
+             e.Graphics.DrawString(" " + textBox7.Text, new Font("Arial", 8, FontStyle.Regular), Brushes.DarkOliveGreen, new Point(25, 10));
+           
+            e.Graphics.DrawString("House Renter's name     : " + comboBox2.Text, new Font("Arial", 12, FontStyle.Bold), Brushes.Black, new Point(25, 60));
+
+            e.Graphics.DrawString("Meter No                          : " + comboBox3.Text, new Font("Arial", 12, FontStyle.Bold), Brushes.Black, new Point(25, 90));
+            e.Graphics.DrawString("Year of " + textBox2.Text, new Font("Arial", 12, FontStyle.Bold), Brushes.Blue, new Point(25, 120));//year
+            e.Graphics.DrawString("Month Name:"+comboBox1.Text, new Font("Arial", 12, FontStyle.Bold), Brushes.Blue, new Point(380, 120));//Month Name
+            e.Graphics.DrawString(DashLabel.Text, new Font("Arial", 12), Brushes.Black, new Point(5, 130));
+
+
+            e.Graphics.DrawString(DashLabel2.Text, new Font("Arial", 12), Brushes.Black, new Point(5, 137));
+            e.Graphics.DrawString("House Rent/Month", new Font("Arial", 12, FontStyle.Bold), Brushes.ForestGreen, new Point(25, 150));
+            e.Graphics.DrawString("Your Current Bill", new Font("Arial", 12, FontStyle.Bold), Brushes.ForestGreen, new Point(200, 150));
+            e.Graphics.DrawString("Previous Due", new Font("Arial", 12, FontStyle.Bold), Brushes.ForestGreen, new Point(400, 150));
+            e.Graphics.DrawString("Payment given At Begining of recent month", new Font("Arial", 10, FontStyle.Bold), Brushes.ForestGreen, new Point(550, 150));
+            e.Graphics.DrawString(DashLabel.Text, new Font("Arial", 12), Brushes.Black, new Point(5, 170));
+            
+            e.Graphics.DrawString("In Tk." + comboBox4.Text, new Font("Arial", 12, FontStyle.Regular), Brushes.Blue, new Point(35, 200));//House rent
+            e.Graphics.DrawString("In Tk." + textBox5.Text, new Font("Arial", 12, FontStyle.Regular), Brushes.Blue, new Point(210, 200));//your Current Bill
+            e.Graphics.DrawString("In Tk." + textBox4.Text, new Font("Arial", 12, FontStyle.Regular), Brushes.Blue, new Point(405, 200));//Previous due
+            e.Graphics.DrawString("In Tk." + textBox8.Text, new Font("Arial", 12, FontStyle.Regular), Brushes.Blue, new Point(650, 210));//Paid at recent month
+            e.Graphics.DrawString(DashLabel2.Text, new Font("Arial", 12), Brushes.Black, new Point(540, 137));
+            e.Graphics.DrawString(DashLabel2.Text, new Font("Arial", 12), Brushes.Black, new Point(535, 137));
+
+            e.Graphics.DrawString(DashLabel.Text, new Font("Arial", 12), Brushes.Black, new Point(5, 250));
+            e.Graphics.DrawString("Rest Total amount to pay  ", new Font("Arial", 12, FontStyle.Bold), Brushes.Indigo, new Point(25, 270));
+            e.Graphics.DrawString(DashLabel.Text, new Font("Arial", 12), Brushes.Black, new Point(5, 297));
+            e.Graphics.DrawString("= In Tk." + textBox6.Text, new Font("Arial", 12, FontStyle.Bold), Brushes.Indigo, new Point(400, 270));//Rest total payment
+            e.Graphics.DrawString("Thank you", new Font("Arial", 12, FontStyle.Regular), Brushes.Black, new Point(350, 330));
+
+            e.Graphics.DrawString(" Issued Date : " + DateTime.Now, new Font("Arial", 8, FontStyle.Regular), Brushes.DarkGreen, new Point(620, 10));
+           
+            
+            Bitmap bmp = Properties.Resources.jamir;
+            Image newImage = bmp;
+            e.Graphics.DrawImage(newImage, 400, 10, newImage.Width, newImage.Height);
+        
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)//to print
+        {
+            HRprintDocument.Print();
+            MessageBox.Show("Pdf file Created. Please Connect the printer & print your File");
+        }
+
+      
+
+      
+
+        private void button5_Click(object sender, EventArgs e)//login
+        {
+
+            SqlConnection con = new SqlConnection("Data Source=.\\SQLEXPRESS;AttachDbFilename=E:\\Csharp\\Form\\House Rent\\House Rent\\shourav.mdf;Integrated Security=True;Connect Timeout=30;User Instance=True");
+            SqlDataAdapter sc = new SqlDataAdapter(" SELECT Count(*) FROM jam Where Username = '" + textBox9.Text + "' and Password = '" + textBox10.Text + "'   ", con);
+            DataTable dt = new DataTable();
+            sc.Fill(dt);
+
+            if (dt.Rows[0][0].ToString() == "1")
+            {
+                this.Hide();
+                Main ss = new Main();
+                ss.Show();
+            }
+
+
+            else
+            {
+                MessageBox.Show("please insert correct password");
+
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)//save to db
+        {
+            SqlConnection con = new SqlConnection("Data Source=.\\SQLEXPRESS;AttachDbFilename=C:\\Users\\Jamir\\Documents\\Data.mdf;Integrated Security=True;Connect Timeout=30;User Instance=True");
+            con.Open();
+
+            SqlCommand sc = new SqlCommand("Insert Rent1 values(" + textBox1.Text + " ," + textBox2.Text + " ,'" + comboBox1.Text + "', '" + comboBox2.Text + "' ," + comboBox4.Text + " ," + textBox5.Text + " ," + textBox4.Text + ", " + textBox6.Text + " ) ", con);
+            object o = sc.ExecuteNonQuery();
+
+            MessageBox.Show(o + " : Record has been inserted");
+            con.Close();
+
+        }
+
+        
+
+
+
+       
+
+       
+
+
+    }
+}
---
+++ House Rent/Main.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace House_Rent
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+
+        SqlConnection con = new SqlConnection("Data Source=.\\SQLEXPRESS;AttachDbFilename=C:\\Users\\Jamir\\Documents\\Data.mdf;Integrated Security=True;Connect Timeout=30;User Instance=True");
+        private void button1_Click(object sender, EventArgs e)//view db
+        {
+            con.Open();
+            SqlDataAdapter sc = new SqlDataAdapter(" SELECT *FROM Rent1 ", con);
+
+            DataTable Data = new DataTable();
+            sc.Fill(Data);
+            dataGridView1.DataSource = Data;
+
+
+            con.Close();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)//delete db
+        {
+            con.Open();
+            SqlDataAdapter sc = new SqlDataAdapter(" DELETE FROM Rent1 ", con);
+            sc.SelectCommand.ExecuteNonQuery();
+
+            con.Close();
+            MessageBox.Show("Delete Successfully");
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)//send to excel
+        {
+            saveFileDialog1.InitialDirectory = "C:";
+            saveFileDialog1.Title = "Save as excel file";
+            saveFileDialog1.FileName = "";
+            saveFileDialog1.Filter = "Excel File (2007)|*.xls|Excel File(2010)|*.xls";
+
+            if (saveFileDialog1.ShowDialog() != DialogResult.Cancel)
+            {
+                Microsoft.Office.Interop.Excel.Application ExcelApp = new Microsoft.Office.Interop.Excel.Application();
+                ExcelApp.Application.Workbooks.Add(Type.Missing);
+
+                ExcelApp.Columns.ColumnWidth = 12;
+
+                for (int i = 1; i < dataGridView1.Columns.Count + 1; i++)
+                {
+
+                    ExcelApp.Cells[1, i] = dataGridView1.Columns[i - 1].HeaderText;
+
+
+                }
+
+
+                for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+                {
+
+
+                    for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                    {
+                        ExcelApp.Cells[i + 2, j + 1] = dataGridView1.Rows[i].Cells[j].Value.ToString();
+
+
+
+
+
+                    }
+
+
+
+                }
+
+
+
+                ExcelApp.ActiveWorkbook.SaveCopyAs(saveFileDialog1.FileName.ToString());
+                ExcelApp.ActiveWorkbook.Saved = true;
+                ExcelApp.Quit();
+
+
+            }
+
+        }
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)//datagrid print document
+        {
+            Bitmap bm = new Bitmap(this.dataGridView1.Width, this.dataGridView1.Height);
+            dataGridView1.DrawToBitmap(bm, new Rectangle(0, 0, this.dataGridView1.Width, this.dataGridView1.Height));
+            e.Graphics.DrawImage(bm, 0, 0);
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)//datagrid print button
+        {
+            printDocument1.Print();
+        }
+
+        private void button5_Click(object sender, EventArgs e)//exit
+        {
+            this.Close();
+        }
+
+        
+    }
+}
